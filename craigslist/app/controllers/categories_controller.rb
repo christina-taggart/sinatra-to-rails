c9590d94371b933ac93862f6926f0dce14719bc9
Apprentice <apprentice@dbc23.local>
2014-02-18 10:16:43 -0800

    Added categories, controller, migration, model and routes

---
+++ craigslist/app/controllers/categories_controller.rb
@@ -0,0 +1,42 @@
+class CategoriesController < ApplicationController
+
+  def index # show all categories
+    @categories = Category.all
+  end
+
+  def show # show category dashboard
+    @category = Category.find(params[:id])
+  end
+
+  def new # create category
+    @category = Category.new
+  end
+
+  def create # post to create category
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(params[:category])
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
+  def destroy # delete category
+    category = Category.find(params[:id])
+    category.destroy
+    redirect_to root_path
+  end
+end
---
+++ craigslist/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craigslist::Application.config.secret_token = 'abcd539331de8e0731d31cf3dd1392c5104421e16dea25352c60bb57a5e62ae66fbcb5a275111045436545ff7a66aa2847add9ab0e5710da71e0835c9cae1df7'
