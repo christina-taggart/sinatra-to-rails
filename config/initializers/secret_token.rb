16f556686adf93e4019acecd0cb18ff5de807df8
Aditya Mahesh & Ricardo A. Rubio <pair+amahesh0501.rickarubio@devbootcamp.com>
2014-02-18 09:33:41 -0800

    generated rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackernewsclone::Application.config.secret_token = '07216ea82406d55820ff536af2b8a21ec57987863d0c0239905c118da87c185dfbe399b759e70356d552150b9a6e89bbf993bff48159a06fe6a3ff2ae533256f'
