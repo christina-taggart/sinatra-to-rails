e22461f1549a5f57a70ef4d27718bd4deb74c78a
Kevin Yeh & Patrick Vilhena <pair+kyeh.nifengle@devbootcamp.com>
2014-02-18 09:37:48 -0800

    created new craigslist_jr rails app

---
+++ craigslist_jr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraigslistJr::Application.config.secret_token = 'c760b71625a9f58521423d817ca2722c5e26819dfbd23ccd1c87778d2ba7881aec02e1bb6c23c4a243c7f79451259484371b68655b232408f219724e69d71ac6'
