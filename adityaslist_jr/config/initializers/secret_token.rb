f8941f599688a0b6552fb52165d94710cdf1fcb4
Aditya Mahesh & Kevin Yeh <pair+amahesh0501.kyeh@devbootcamp.com>
2014-02-19 15:31:20 -0800

    Add rails project

---
+++ adityaslist_jr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdityaslistJr::Application.config.secret_token = '49a6920180dda87b6e2faeb16fe7b8679e10c3f92832f7b2eac242571b8dde2036d624d8a0b44523f2d9e0ca212be473fa8a149573a407d247a41b0bdd7787a9'
