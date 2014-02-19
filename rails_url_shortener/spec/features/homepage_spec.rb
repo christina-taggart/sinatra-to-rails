c96d124cbef846641f06f969087f1ef4f588a6ff
Emily Foley <null>
2014-02-19 12:22:03 -0800

    refactored features tests with before each for readability and DRYness

--- rails_url_shortener/spec/features/homepage_spec.rb
+++ rails_url_shortener/spec/features/homepage_spec.rb
@@ -5,26 +5,26 @@ describe "homepage" do
   let!(:url) { Url.create :long_url => long_url }
   let(:short_url) { url.short_url }
   context "list of urls" do
-    it "displays the long urls" do
+    before :each do
       visit root_path
+    end
+    it "displays the long urls" do
       expect(page).to have_content (long_url)
     end
     it "displays the short urls" do
-      visit root_path
       expect(page).to have_content (short_url)
     end
   end
   context "displaying user input on homepage" do
-    it "appends the long url that the user typed in to the page" do
+    before :each do
       visit root_path
       fill_in "url[long_url]", :with => long_url
       click_on "Create Url"
+    end
+    it "appends the long url that the user typed in to the page" do
       expect(page).to have_content (long_url)
     end
     it "appends the short url that is created from the long url to the page" do
-      visit root_path
-      fill_in "url[long_url]", :with => long_url
-      click_on "Create Url"
       expect(page).to have_content (short_url)
     end
   end
